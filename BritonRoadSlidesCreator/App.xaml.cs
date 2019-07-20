b6bcab16c7b91e878a0a4d7e2e99506da5e036ea
Gregory Loomis <cmusicfan2000@gmail.com>
2019-07-20 15:56:47 -0500

    Created initial project and viewmodels project

--- BritonRoadSlidesCreator/App.xaml.cs
+++ BritonRoadSlidesCreator/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Unity;
 
 namespace BritonRoadSlidesCreator
 {
@@ -13,5 +14,14 @@ namespace BritonRoadSlidesCreator
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IUnityContainer container = new UnityContainer();
+            //container.RegisterType<ICustomerService, CustomerService>();
+            //container.RegisterType<IShoppingCartService, ShoppingCartService>();
+
+            MainWindow mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
